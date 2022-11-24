ac014417c696434610eb501a6f251e88e47b7097
Eka Aenun Nisa <anastasya.nisa57@gmail.com>
2022-11-24 21:07:32 +0700

    Initial commit

---
+++ app/src/main/java/com/example/toolbarnavigationui/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.toolbarnavigationui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.toolbarnavigationui.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var navController: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.containerNavHostFragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+
+        // setup toolbar
+        setupToolbar()
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    private fun setupToolbar(){
+        setSupportActionBar(binding.toolbar)
+    }
+}
